46a35428719053d963c5be6e5e6136ed143bcaad
Angel Saenz Badillos <ramonsb@yahoo.com>
2026-04-02 19:13:35 -0700

    Initial Commit

---
+++ KitchenInventory LLM/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  KitchenInventory LLM
+//
+//  Created by Angel on 4/2/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
